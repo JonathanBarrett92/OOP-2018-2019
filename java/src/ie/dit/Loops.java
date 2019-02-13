f1ca2d62b2146ddc3bb81eaf5c8a58f34847577b
JonathanBarrett92 <jobarret@tcd.ie>
2019-02-13 17:32:15 +0000

    Exercises done

--- java/src/ie/dit/Loops.java
+++ java/src/ie/dit/Loops.java
@@ -14,7 +14,7 @@ public class Loops extends PApplet
 		dy = 0;
 		cx = width / 2;
 		cy = height / 2;
-
+		colorMode(HSB, 100);
 	}
 
 	/*
@@ -78,23 +78,65 @@ public class Loops extends PApplet
 		}
 		*/
 
-		float gap = height / 10;
-		for(int lines = 0; lines < 10 ; lines++)
+		float gap = height / 25;
+		for(int lines = 0; lines < 26 ; lines++)
 		{
 			line(0, lines * gap, width, lines * gap);
 		}
 	}
 
-	void loops1()
+	void exercise1()
 	{
-		int numLines = 20;
+		int numLines = 25;
 		float gap = width / numLines;
+		float gaph = height / numLines;
 
 		for(int l = 0 ; l < numLines; l ++)
 		{
 			float x = l * gap;
 			line(x, 0, width - x, height);
 		}
+
+		for(int l = 0 ; l < numLines; l ++)
+		{
+			float x = l * gaph;
+			line(0, x, height, width - x);
+		} 
+	}
+
+	void exercise2(int circleSize, int amountOfCircles){
+		for (int i = 0; i < amountOfCircles; i++) {
+			ellipse((circleSize/2) + (i*circleSize), (height/2), circleSize, circleSize);	
+		}
+		
+	}
+
+	void exercise3(int numRects){
+		int w = width/numRects;
+	
+		for (int i = 0; i < numRects; i++) {
+			int gap = i*w;
+			fill(i*10, 100, 100);
+			rect(gap, 0, gap+w, height);
+
+		}
+	}
+
+	void exercise4(int circleSize, int amountOfCircles){
+		for (int i = 0; i < amountOfCircles; i++) {
+			fill((i*10), 100, 100);
+			ellipse((circleSize/2) + (i*circleSize), (height/2), circleSize, circleSize);	
+		}
+	}
+
+	void exercise5(int circleSize, int amountOfCircles){
+		for (int i = 0; i < amountOfCircles; i++) {
+			for (int j = 0; j < amountOfCircles; j++) {
+				fill((i*5)+(j*5), 100, 100);
+				ellipse((circleSize/2) + (i*circleSize), (circleSize/2) + (j*circleSize), circleSize, circleSize);
+			}	
+		}
+
 	}
 
 
@@ -103,14 +145,23 @@ public class Loops extends PApplet
 		background(0);
 		stroke(255);
 		noFill();	
-		drawFace(cx + dx, cy + dy, 70);
-		drawFace(cx + dx, cy -dy, 70);
-		drawFace(cx - dx, cy + dy, 70);
-		drawFace(cx - dx, cy -dy, 70);
+		// drawFace(cx + dx, cy + dy, 70);
+		// drawFace(cx + dx, cy -dy, 70);
+		// drawFace(cx - dx, cy + dy, 70);
+		// drawFace(cx - dx, cy -dy, 70);
 
-		dx ++;
-		dy ++;
-		loops();
-		loops1();
+		// dx ++;
+		// dy ++;
+		// loops();
+		
+		// exercise1();
+		//exercise2(width/10, 10);
+
+		//exercise3(10);
+
+		//exercise4(width/10, 10);
+
+		exercise5(width/10, 10);
+
 	}
 }
